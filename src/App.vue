67b36f69c5be4f4b24460ff622273d4a2380980b
hmhorstmann <hiagohorstmann@gmail.com>
2023-05-21 16:50:44 -0300

    chore: define ccs global e fonte

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,23 @@
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: '#E5E5E5';
+  color: '#000000';
+  -webkit-font-smoothing: 'antialiased';
+}
+body,
+input,
+textarea,
+button {
+  font-family: 'Fira Code', monospace;
+  font-weight: 400;
+}
 </style>
