8366eee8706d0ccc916622d0478d8dad9a957d89
hmhorstmann <hiagohorstmann@gmail.com>
2023-05-21 20:09:31 -0300

    chore: instala biblioteca primevue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./routes/index"
+import PrimeVue from "primevue/config"
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 app.mount('#app')
