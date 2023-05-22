9dffa1b914a4e8bb57ad69f5ad3bec565b50f951
hmhorstmann <hiagohorstmann@gmail.com>
2023-05-22 18:50:45 -0300

    feat: adiciona referência no carrossel dos personagens, reconstroi a chamada da api com axios e cria carregamento dinâmico dos personagens paginados

--- src/pages/Movies.vue
+++ src/pages/Movies.vue
@@ -1,15 +1,15 @@
 <template>
   <Divider title="Movies"></Divider>
   <Carousel
+    :modelValue="1"
     :itemsToShow="3"
     :wrapAround="true"
-    :autoplay="3000"
+    :autoplay="0"
     :pauseAutoplayOnHover="true"
   >
     <Slide v-for="film in films" :key="film.episode_id">
       <div class="carousel__item">
         <CardFilm :film="film"></CardFilm>
-        
       </div>
     </Slide>
 
@@ -19,7 +19,12 @@
   </Carousel>
 
   <Divider title="Characters"></Divider>
-  <Carousel :itemsToShow="3" :wrapAround="true">
+  <Carousel
+    ref="peopleCarousel"
+    :itemsToShow="3"
+    :wrapAround="true"
+    @slide-end="loop($event)"
+  >
     <Slide v-for="person in people" :key="person.name">
       <div class="carousel__item">
         <CardPeople :person="person"></CardPeople>
@@ -35,7 +40,8 @@
 <script lang="ts">
   import { defineComponent, onMounted, ref } from 'vue'
   import Divider from '../components/Divider.vue'
-  import { API } from '../services/axios'
+  // import { API } from '../services/axios'
+  import axios from 'axios'
   import { Film, FilmResponse } from '../interfaces/FilmInterface'
   import { Person, PersonResponse } from '../interfaces/PersonInterface.js'
   import CardFilm from '../components/CardFilm.vue'
@@ -58,32 +64,61 @@
       const films = ref<Film[]>([])
       const people = ref<Person[]>([])
 
-      const getFilms = async () => {
+      const peopleCarousel = ref<any>()
+
+      const getFilms = async (): Promise<Film[]> => {
         try {
-          const response = await API.get<FilmResponse>('films')
-          films.value = response.data.results
-          console.log('films', films.value)
+          const response = await axios.get<FilmResponse>(
+            'https://swapi.dev/api/films/'
+          )
+          return response.data.results
+          // films.value = response.data.results
         } catch (error) {
           console.error(error)
+          throw error
         }
       }
 
-      const getPeople = async () => {
+      const getPeople = async (page: number = 1): Promise<Person[]> => {
         try {
-          const response = await API.get<PersonResponse>('people')
-          people.value = response.data.results
-          console.log('people', people.value)
+          const response = await axios.get<PersonResponse>(
+            `https://swapi.dev/api/people/?page=${page}`
+          )
+          loadedPages.value.push(page)
+          data.value = response.data
+          return response.data.results
+          // people.value = response.data.results
         } catch (error) {
           console.error(error)
+          throw error
         }
       }
 
       onMounted(async () => {
-        await getFilms()
-        await getPeople()
+        films.value = await getFilms()
+        people.value = await getPeople()
       })
 
-      return { films, people }
+      const loadedPages = ref<number[]>([])
+      const data = ref<PersonResponse>()
+
+      const loop = async (param: any) => {
+        if (param.currentSlideIndex == param.slidesCount - 3) {
+          const url = data.value?.next
+          const arrayUrl = url?.split('page=')
+          const pageNumber = arrayUrl?.length ? parseInt(arrayUrl[1]) : 1
+
+          // const count = data.value?.count
+          // const arroundCount = count ? Math.ceil(count / 10) : 0
+
+          if (!loadedPages.value.includes(pageNumber)) {
+            const newPage = await getPeople(pageNumber)
+            people.value = people.value.concat(newPage)
+          }
+        }
+      }
+
+      return { films, people, peopleCarousel, loop }
     },
   })
 </script>
