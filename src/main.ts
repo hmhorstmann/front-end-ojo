45c2c9c5d5c4241975a884428e8b9377e36855a6
hmhorstmann <hiagohorstmann@gmail.com>
2023-05-22 21:34:31 -0300

    feat: inicializa o pinia

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./routes/index"
 import PrimeVue from "primevue/config"
+import { createPinia } from 'pinia'
+// import { useReviewStore } from './stores/reviewStore'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
 app.use(PrimeVue)
+app.use(pinia)
+// pinia.use(useReviewStore)
 app.mount('#app')
